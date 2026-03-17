3333c757412f4d6a94ca6287fadefdb9192f0b3d
9Rish-create <arinastepanets@yandex.ru>
2026-03-17 21:20:16 +0300

    update auth.component

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @Component({
   selector: 'app-auth',
-  imports: [AuthorizationComponent, RegistrationComponent], //сюда нужно вписывать названия классов из дочерних компонентов; импорт будет прописываться автоматически
+  imports: [AuthorizationComponent, RegistrationComponent, MatTabsModule], //сюда нужно вписывать названия классов из дочерних компонентов; импорт будет прописываться автоматически
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })

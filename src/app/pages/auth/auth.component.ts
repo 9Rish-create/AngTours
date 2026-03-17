3f8f17baf4558e8a0702cc1c0881db5c09b46624
9Rish-create <arinastepanets@yandex.ru>
2026-03-17 20:38:51 +0300

    update app.routes + some comments

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [AuthorizationComponent, RegistrationComponent], //сюда нужно вписывать названия классов из дочерних компонентов; импорт будет прописываться автоматически
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })

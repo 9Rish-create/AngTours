a3f6bd51a0103ac49be431830304a6d7d8732ddb
9Rish-create <arinastepanets@yandex.ru>
2026-03-28 20:32:52 +0300

    add lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,20 @@ import { AuthComponent } from './pages/auth/auth.component';
 import {  LayoutComponent } from './layout/layout.component'
 
 export const routes: Routes = [
-    {path: '', component: LayoutComponent},
-    {path: 'auth', component: AuthComponent,
-    }
+    
+    {
+        path: 'auth', 
+        loadComponent: () => import('./pages/auth/auth.component').then(c => c.AuthComponent)
+
+    },
+
+    {
+        path: '', 
+        component: LayoutComponent},
+
+    // {path: '',
+    //    loadComponent: () => import('./layout/layout.component').then(c => c.LayoutComponent) 
+    // } динамическая загрузка
+    
     //здесь хранится связь между запросом в адресной строке и компонентом, который должен отобразиться!
 ];
